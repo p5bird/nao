ba79ed700a2d0f38d4a8d05327a50a5d3d6418e6
gatien <gatienhrd@gmail.com>
2018-02-01 16:26:45 +0100

    Added UserController, Dashboard views and route

---
+++ src/UserBundle/Controller/UserController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saiya
+ * Date: 2/1/18
+ * Time: 1:59 PM
+ */
+
+namespace UserBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Application controller
+ *
+ * @Route("/dashboard")
+ */
+class UserController extends Controller {
+
+    /**
+     * Dashboard
+     *
+     * @Route("/", name="nao_dashboard")
+     * @return Response
+     */
+    public function dashboardAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        return $this->render('UserBundle:User:dashboard.html.twig');
+    }
+}
