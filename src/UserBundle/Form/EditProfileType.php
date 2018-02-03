0760099da37ed65c9c52cc3014d99f0dab07e34c
gatien <gatienhrd@gmail.com>
2018-02-03 19:33:48 +0100

    Added required false to EditProfileType options

--- src/UserBundle/Form/EditProfileType.php
+++ src/UserBundle/Form/EditProfileType.php
@@ -14,7 +14,6 @@ use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 use FOS\UserBundle\Form\Type\ProfileFormType as BaseProfileFormType;
 
@@ -23,6 +22,7 @@ class EditProfileType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add('firstname', TextType::class, array(
+                'required' => false,
                 'label' => 'Prénom',
                 'attr' => [
                     'class' => 'form-control',
@@ -31,6 +31,7 @@ class EditProfileType extends AbstractType {
                 'error_bubbling' => true
             ))
             ->add('name', TextType::class, array(
+                'required' => false,
                 'label' => 'Nom',
                 'attr' => [
                     'class' => 'form-control',
@@ -39,6 +40,7 @@ class EditProfileType extends AbstractType {
                 'error_bubbling' => true
             ))
             ->add('city', TextType::class, array(
+                'required' => false,
                 'label' => 'Ville',
                 'attr' => [
                     'class' => 'form-control',
@@ -47,6 +49,7 @@ class EditProfileType extends AbstractType {
                 'error_bubbling' => true
             ))
             ->add('level', TextType::class, array(
+                'required' => false,
                 'label' => 'Niveau ornithologique',
                 'attr' => [
                     'class' => 'form-control',
@@ -55,14 +58,16 @@ class EditProfileType extends AbstractType {
                 'error_bubbling' => true
             ))
             ->add('birthdate', BirthdayType::class, array(
-            'label' => 'Date de naissance',
-            'attr' => [
-                'class' => 'form-control',
-                'placeholder' => 'Date de naissance',
-            ],
-            'error_bubbling' => true
+                'required' => false,
+                'label' => 'Date de naissance',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Date de naissance',
+                ],
+                'error_bubbling' => true
         ))
             ->add('job', TextType::class, array(
+                'required' => false,
                 'label' => 'Profession',
                 'attr' => [
                     'class' => 'form-control',
@@ -71,14 +76,16 @@ class EditProfileType extends AbstractType {
                 'error_bubbling' => true
             ))
             ->add('facebook', UrlType::class, array(
-            'label' => 'Facebook',
-            'attr' => [
-                'class' => 'form-control',
-                'placeholder' => 'Facebook',
-            ],
-            'error_bubbling' => true
+                'required' => false,
+                'label' => 'Facebook',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Facebook',
+                ],
+                'error_bubbling' => true
         ))
             ->add('pinterest', UrlType::class, array(
+                'required' => false,
                 'label' => 'Pinterest',
                 'attr' => [
                     'class' => 'form-control',
@@ -87,6 +94,7 @@ class EditProfileType extends AbstractType {
                 'error_bubbling' => true
             ))
             ->add('instagram', UrlType::class, array(
+                'required' => false,
                 'label' => 'Instagram',
                 'attr' => [
                     'class' => 'form-control',
@@ -95,6 +103,7 @@ class EditProfileType extends AbstractType {
                 'error_bubbling' => true
             ))
             ->add('bio', TextAreaType::class, array(
+                'required' => false,
                 'label' => 'Biographie',
                 'attr' => [
                     'class' => 'form-control',
@@ -103,6 +112,7 @@ class EditProfileType extends AbstractType {
                 'error_bubbling' => true
             ))
             ->add('avatar', AvatarType::class, array(
+                'required' => false,
                 'label' => 'Avatar',
                 'attr' => [
                     'class' => 'form-control',
