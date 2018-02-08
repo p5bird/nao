dd83c223e8ef473101b860fc53ed93023ed326d6
JoKolov <joffreynicoloff@gmail.com>
2018-02-08 11:13:49 +0100

    Initializing ObservationBundle - ObservationBundle added - Routes added in app/config - 5 views added in ObservationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
+            new ObservationBundle\ObservationBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
