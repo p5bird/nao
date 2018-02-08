b366086aff650eb62b7fcde2d5ae7fe1c5c7e47d
gatien <gatienhrd@gmail.com>
2018-02-08 18:20:14 +0100

    Add user menu, CSS additions

--- web/js/toggle-sidebar.js
+++ web/js/toggle-sidebar.js
@@ -4,9 +4,15 @@ $( document ).ready(function() {
         if ($('#sidebar.col-md-2').length !== 0) {
             $('#sidebar').addClass('col-md-1').removeClass('col-md-2');
             $('#user-interface-page-content').addClass('col-md-11').removeClass('col-md-10');
+            $('i.fas.fa-angle-right').css('display', 'none');
+            $('.navbar-user-username').css('padding', '13px 0 0 30px');
+            $('.navbar-user-group').css('padding-left', '30px')
         } else {
             $('#sidebar').removeClass('col-md-1').addClass('col-md-2');
             $('#user-interface-page-content').removeClass('col-md-11').addClass('col-md-10');
+            $('i.fas.fa-angle-right').css('display', 'inline-block');
+            $('.navbar-user-username').css('padding', '13px 0 0 0');
+            $('.navbar-user-group').css('padding-left', '0px')
         }
     });
 
